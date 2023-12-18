46a29c40796fa9230d88b0dee884f5342e2e76d0
Max Bakalets <max.bakalets@gmail.com>
2023-12-18 22:38:59 +0200

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,9 @@
 function sum(a, b) {
-  /* ваш код */
-}
+  if (typeof a !== "number" || typeof b !== "number") {
+    return new "TypeError";
+  } else {
+    return a + b;
+  };
+};
 
 module.exports = sum;
